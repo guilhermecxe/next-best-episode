dec532b831bc2a40a097cc810d7acf892b3a5904
Guilherme Alves <guilhermecxe@hotmail.com>
2023-06-26 08:44:46 -0300

    Revert "first commit"
    This reverts commit 0e0aea50a502754a59f97c2247c849dafa5b42a4.

--- result.jl
+++
@@ -1,3 +0,0 @@
-{"tv_show": "Game of Thrones", "episode": "S01E01", "rating": 8.9}
-{"tv_show": "Lucifer", "episode": "S01E01", "rating": 8.5}
-{"tv_show": "Stranger Things", "episode": "S01E01", "rating": 8.5}
